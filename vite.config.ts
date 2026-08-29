b88a609f24497da1a1a4265c43610b4d77529085
Andrew Witwicki <vitvitskiy3@gmail.com>
2026-08-29 09:31:24 +0200

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === 'true' ? '/SDRLab/' : '/',
   plugins: [react()],
+  worker: {
+    format: 'es',
+  },
   test: {
     environment: 'jsdom',
     globals: true,
